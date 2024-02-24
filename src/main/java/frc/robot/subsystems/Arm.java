731bbb724b91c17cb2fc8c5ecbc404652e75235e
Humans4501 <nrsrobotics@gmail.com>
2024-02-24 15:04:35 -0800

    Add encoder diagnostics

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -11,14 +12,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * Arm subsystem
  */
 public class Arm extends SubsystemBase {
-	private final int kArmLeftCanID = 40;
-	private final int kArmRightCanID = 41;
+	private final int kArmLeftCanID = 35;
+	private final int kArmRightCanID = 34;
 
 	private final double kPosOffset = 0.0;
 
 	/** Follows the right motor controller */
 	private final CANSparkMax mArmLeft = new CANSparkMax(kArmLeftCanID, MotorType.kBrushless);
 	private final CANSparkMax mArmRight = new CANSparkMax(kArmRightCanID, MotorType.kBrushless);
+	private final DutyCycleEncoder mEncoder = new DutyCycleEncoder(0);
 
 	public Arm() {
 		this.mArmLeft.follow(this.mArmRight, true);
@@ -37,5 +39,10 @@ public class Arm extends SubsystemBase {
 
 			this.mArmRight.set(set);
 		});
+	}
+
+	@Override
+	public void periodic() {
+		SmartDashboard.putNumber("Encoder", this.mEncoder.getAbsolutePosition() + kPosOffset);
 	}
 }
